665032cbbafe82143ef7fa08278604a02b988517
SZ-Agent <seanauditor0704@gmail.com>
2023-03-09 01:47:00 -0600

    first commits

---
+++ 1-intro.js
@@ -0,0 +1,13 @@
+/*
+first program in node (well, not really, just trying things out step-by-step)
+*/ 
+
+const amount = 12
+
+if(amount < 10){
+    console.log("small number")
+} else{
+    console.log("large number")
+}
+
+console.log(`first node app`)
---
+++ 2-globals.js
@@ -0,0 +1,7 @@
+// GLOBALS - NO WINDOW OBJECT //
+
+// __dirname - path to current directory
+// __filename - file name
+// require - funciton to use modules, just like import
+// module - info about current module
+// process - info about env where the program is being executed
